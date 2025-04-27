c5ef2a4bd94807a9c843def351440c8c32923876
youngvan12845 <yangfan12845@outlook.com>
2025-04-27 16:45:30 +0800

    final

--- a_blog/urls.py
+++ a_blog/urls.py
@@ -3,8 +3,11 @@ from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.documents import urls as wagtaildocs_urls
 from wagtail import urls as wagtail_urls
 
+from .views import *
 urlpatterns = [
     path('cms/', include(wagtailadmin_urls)),
     path('documents/', include(wagtaildocs_urls)),
+    path('search/',article_search, name='article_search'),
     path('', include(wagtail_urls)),
 ]
+
--- a_blog/views.py
+++ a_blog/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
+from .models import ArticlePage
+
 # Create your views here.
+def article_search(request):
+    search_query = request.GET.get('query','').strip()
+    if search_query:
+        articles = ArticlePage.objects.live().search(search_query)
+        articles = sorted(articles, key=attrgetter('first_published_at'),reverse=True)
+    else:
+        articles = ArticlePage.objects.live().order_by('-first_published_at')
+    context = {
+        'articles': articles,
+        'search_query': search_query,
+    }
+    return render(request, 'a_blog/blog_page.html', context)
+
